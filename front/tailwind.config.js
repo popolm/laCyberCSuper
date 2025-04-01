bef806bdb823ba9d2d02292cc2cfedba85c231b1
Pauline <lmg.pauline1@gmail.com>
2025-04-01 13:46:04 +0200

    refresh

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -1,12 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./*.html", "./src/**/*.{js,ts,jsx,tsx}", "./style.css"],
   theme: {
-    extend: {
-    },
+    extend: {},
   },
   plugins: [],
-}
+};
